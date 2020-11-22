721b2fc3e8a00e7eba16e7993cb6e140aa775195
NandishKumar178 <74757255+NandishKumar178@users.noreply.github.com>
2020-11-22 08:42:03 +0000

    Initial solution setup

---
+++ Mycart/Mycart.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mycart.WebUI.Startup))]
+namespace Mycart.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
